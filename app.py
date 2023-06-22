c58aff95df8da22ff975bff428b0153cc9d7d67d
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-06-21 22:39:12 -0300

    ploty

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 
 #libs gráficas
-import plotly.express as px
 import plotly.graph_objects as go 
 from plotly.subplots import make_subplots
 import seaborn as sns
