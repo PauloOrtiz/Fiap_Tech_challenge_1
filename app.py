988279ad1de3150e1033a0890f6bf0834bcefb59
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-06-24 14:37:37 -0300

    update

--- app.py
+++ app.py
@@ -15,67 +15,62 @@ from PIL import Image
 
 #Lendo a base de dados e tratanto a base de dados
 df_resultado = pd.read_csv('./src/data/resultado.csv')
-
+df = pd.read_csv('./src/data/ExpVinho.csv', sep=";")
 
 
 #Titulo de Página
 image = Image.open("./src/img/download.jpg")
 st.image(image)
-st.title('Estudo sobre exportação de vinhos entre 2007 a 2021')
 
 # Layout do aplicativo
 tab0, tab1, tab2, tab3= st.tabs(["Inicio","Dados Gerais", "Paises", "Sobre"])
 
 with tab0:
 
-    # Título da apresentação
-    st.title("Relatório de Exportação de Vinho")
+    st.title("Análise da Exportação de Vinhos Brasileiros")
+        
+    st.write("""
+        # Bem-vindos à análise de exportação de vinhos do Brasil
+        Nosso país tem uma rica tradição na produção de vinhos finos e, ao longo dos anos, temos exportado nossos produtos para todo o mundo. Através desta plataforma, estamos empolgados em compartilhar uma análise abrangente da quantidade de vinhos que exportamos e os fatores externos que influenciam essa exportação.
 
-    # Introdução
-    st.markdown("Este relatório tem como objetivo fornecer informações sobre a exportação de vinho nos últimos 15 anos, \
-                visando auxiliar investidores interessados no setor vitivinícola brasileiro.")
+        ## O que vamos abordar
+        - **Quantidade de Vinhos Exportados**: Uma análise ano a ano da quantidade de vinhos exportados para diferentes países. Iremos analisar os padrões de exportação nos últimos 15 anos.
+        - **Fatores Externos que Influenciam a Exportação**: Uma análise profunda de como os dados climáticos, demográficos e econômicos influenciam as exportações de vinho.
+        - **Prospecções Futuras**: Com base nos dados históricos e fatores externos, faremos previsões informadas sobre o futuro das exportações de vinhos do Brasil.
+        - **Estratégias para Melhoria das Exportações**: Com base em nossa análise, vamos sugerir ações estratégicas que podem ser tomadas para melhorar as exportações.
 
-    # Breve visão geral do setor
-    st.header("Visão Geral do Setor")
-    st.markdown("O setor vitivinícola brasileiro tem apresentado um crescimento significativo nas exportações de vinho. \
-                A seguir, apresentamos alguns destaques:")
-
-    # Estatísticas rápidas
-    st.subheader("Estatísticas Rápidas")
-    st.markdown("- O Brasil é um importante exportador de vinhos, com destaque para sua produção de vinhos finos.")
-    st.markdown("- Os principais destinos de exportação incluem países como Estados Unidos, Reino Unido e China.")
-    st.markdown("- O setor tem experimentado um crescimento anual médio de X% nos últimos 15 anos.")
-
-    # Gráfico de linha com tendência de exportação ao longo do tempo
-    st.subheader("Tendência de Exportação de Vinho ao Longo do Tempo")
-    # Insira aqui o código para criar um gráfico de linha com a tendência de exportação de vinho ao longo dos anos
-
-    # Gráfico de barras com principais destinos de exportação
-    st.subheader("Principais Destinos de Exportação de Vinho")
-    # Insira aqui o código para criar um gráfico de barras com os principais destinos de exportação de vinho
-
-    # Conclusão e chamada para ação
-    st.markdown("Com base nessas informações iniciais, é possível observar o potencial de investimento no setor vitivinícola \
-                brasileiro. Recomenda-se uma análise mais detalhada para identificar oportunidades específicas de investimento.")
-
-    # Seção para análise mais detalhada e recomendações
-    st.header("Análise Detalhada e Recomendações")
-    # Insira aqui a análise detalhada dos dados e as recomendações específicas para os investidores
-
-    # Nota de rodapé
-    st.markdown("*Dados fornecidos pelo Vitibrasil - Embrapa Uva e Vinho")
+        ## Nossa Jornada
+        Embarque conosco nesta jornada fascinante através dos dados de nossas exportações de vinhos. Descubra as histórias por trás dos números, entenda os fatores que impulsionam nossas vendas e junte-se a nós enquanto traçamos o caminho para um futuro ainda mais brilhante para a indústria vinícola do Brasil.
+        """)
 
 
 
 with tab1:
+    
 
-    '''
-    ## Pagina2
-    
-    
-    
-    '''
+    # Removendo a coluna 'Id'
+    df.drop('Id', axis=1, inplace=True)
 
+    # Tratando o dataset
+    anos = list(range(1970, 2022))
+    dados = []
+    for ano in anos:
+        df_ano = df.copy()
+        df_ano['Ano'] = ano
+        df_ano['Quantidade'] = df[str(ano)].astype(float)  # convertendo kg em litros (considerando 1kg=1L)
+        df_ano['Valor'] = df[str(ano)].astype(float)
+        dados.append(df_ano[['País', 'Ano', 'Quantidade', 'Valor']])
+
+    df_final = pd.concat(dados)
+
+    # Adicionando a coluna 'Origem'
+    df_final['Origem'] = 'Brasil'
+
+    # Reordenando as colunas
+    df_final = df_final[['Origem', 'País', 'Ano', 'Quantidade', 'Valor']]
+
+    
+    st.dataframe(df_final)  # Mostra o dataframe no Streamlit
 
 
 with tab2:
